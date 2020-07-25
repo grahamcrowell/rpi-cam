cfba490e3f31a526e9f7847ecb322021e9253296
grahamcrowell <graham.crowell@gmail.com>
2020-07-25 18:04:18 -0400

    update vars for rpi

--- scripts/sync-loop.sh
+++ scripts/sync-loop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SYNC_FOLDER=/home/pi/Pictures/nico
-SYNC_FOLDER=mock-sync-folder
+# SYNC_FOLDER=mock-sync-folder
 S3_TARGET_PATH=s3://grahamcrowell.com/nico
 AWD_PROFILE=personal
 SYNC_FILE=latest.jpg
