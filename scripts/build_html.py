8939a4236664ad46ef929f56d64e8127d9453bbf
grahamcrowell <graham.crowell@gmail.com>
2020-07-25 17:49:58 -0400

    set out to pi pics folder

--- scripts/build_html.py
+++ scripts/build_html.py
@@ -6,11 +6,11 @@ from datetime import datetime
 template = """
 <title>Nico Cam</title>
 <h1>${datetime}</h1>
-<img src="http://grahamcrowell.com/nico/latest.jpg" width="500">
+<img src="http://grahamcrowell.com/nico/latest.jpg" width="1000">
 """
 
 dst = "/home/pi/Pictures/nico/index.html"
-dst = "mock-sync-folder"
+# dst = "mock-sync-folder"
 
 with open(dst, 'w') as out:
     mytemplate = Template(template)
