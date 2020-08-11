c429fd250fa4e1754c14545029fde1872898a4c8
grahamcrowell <graham.crowell@gmail.com>
2020-08-10 22:31:26 -0400

    fix paths

--- main-capture.sh
+++ main-capture.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
+echo "$0"
 source .env
 DURATION_MS=$(( $DURATION_SECONDS * 1000 ))
 TIMESTAMP=$(date --utc --iso-8601=seconds)
 
-if [ ! -d "${RAW_VIDEO_PATH}" ]; then
-    echo "create ${RAW_VIDEO_PATH}"
-    mkdir ${RAW_VIDEO_PATH}
+if [ ! -d "${LOCAL_RAW_VIDEO_PATH}" ]; then
+    echo "create ${LOCAL_RAW_VIDEO_PATH}"
+    mkdir ${LOCAL_RAW_VIDEO_PATH}
 fi
 
 echo "capturing..."
 # https://www.raspberrypi.org/documentation/usage/camera/raspicam/raspivid.md
-raspivid -o "${RAW_VIDEO_PATH}/${TIMESTAMP}.h264" -t ${DURATION}
+raspivid --output "${LOCAL_RAW_VIDEO_PATH}/${TIMESTAMP}.h264" --timeout ${DURATION}
 echo "capture complete."
