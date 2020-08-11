7cf37444ccc9b3f94fec290363424a981d072cbb
grahamcrowell <graham.crowell@gmail.com>
2020-08-10 22:17:27 -0400

    split into 2 mains

--- main-capture.sh
+++ main-capture.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-echo "starting main"
-
-DURATION_MS=$((5*1000))
+source .env
+DURATION_MS=$(( $DURATION_SECONDS * 1000 ))
 TIMESTAMP=$(date --utc --iso-8601=seconds)
-RAW_VIDEO_PATH=~/raw-video
 
 if [ ! -d "${RAW_VIDEO_PATH}" ]; then
     echo "create ${RAW_VIDEO_PATH}"
     mkdir ${RAW_VIDEO_PATH}
 fi
 
+echo "capturing..."
 # https://www.raspberrypi.org/documentation/usage/camera/raspicam/raspivid.md
 raspivid -o "${RAW_VIDEO_PATH}/${TIMESTAMP}.h264" -t ${DURATION}
-echo "main complete"
+echo "capture complete."
---
+++ main-sync.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo "$0"
+source .env
+
+# aws --profile ${AWD_PROFILE} s3 ls ${S3_RAW_VIDEO_PATH}
+aws --profile ${AWD_PROFILE} s3 sync ${LOCAL_RAW_VIDEO_PATH} ${S3_RAW_VIDEO_PATH}
