a865070d8001eb891e237c88a4c6028c07f4be9a
grahamcrowell <graham.crowell@gmail.com>
2020-08-10 22:37:05 -0400

    simplier

--- main.sh
+++ main.sh
@@ -7,10 +7,10 @@ CAPTURE_NUM=CAPTURE_ITERS;
 
 function capture-loop() {
     while true; do
-        ./main-capture.sh &
-        capture_pid=$!
-        wait $capture_pid
-        if (( $CAPTURE_NUM < $CAPTURE_ITERS )); then
+        ./main-capture.sh
+        # capture_pid=$!
+        # wait $capture_pid
+        if (( $CAPTURE_NUM <= 0 )); then
             break
         else
             CAPTURE_NUM=$(( $CAPTURE_NUM - 1 ))
@@ -25,10 +25,10 @@ SYNC_NUM=SYNC_ITERS;
 
 function sync-loop() {
     while true; do
-        ./main-sync.sh &
-        sync_pid=$!
-        wait $sync_pid
-        if (( $SYNC_NUM < $SYNC_ITERS )); then
+        ./main-sync.sh
+        # sync_pid=$!
+        # wait $sync_pid
+        if (( $SYNC_NUM < 0 )); then
             break
         else
             SYNC_NUM=$(( $SYNC_NUM - 1 ))
