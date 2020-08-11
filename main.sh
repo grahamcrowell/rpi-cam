895c4dfaca0efda34065d68215276c9c3665806f
grahamcrowell <graham.crowell@gmail.com>
2020-08-10 21:44:34 -0400

    record video

--- main.sh
+++ main.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
-
 echo "starting main"
-sleep 5;
+
+DURATION=5
+TIMESTAMP=$(date --utc --iso-8601=seconds)
+RAW_VIDEO_PATH=~/raw-video
+
+if [ ! -d "${RAW_VIDEO_PATH}" ]; then
+    echo "create ${RAW_VIDEO_PATH}"
+    mkdir ${RAW_VIDEO_PATH}
+fi
+
+# https://www.raspberrypi.org/documentation/usage/camera/raspicam/raspivid.md
+raspivid -o "${RAW_VIDEO_PATH}/${TIMESTAMP}.h264" -t ${DURATION}
 echo "main complete"
